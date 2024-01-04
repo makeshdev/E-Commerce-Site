6b74fa5e1be9e264ae0449dbc7e016aa8187fc1b
makeshkannan1 <makeshkannan@ikm.gov.in>
2024-01-04 19:06:04 +0530

    Latest Footer design

--- src/login/index.jsx
+++ src/login/index.jsx
@@ -1,7 +1,100 @@
-import React from "react";
+import Header from "../common/components/Header";
+import Footer from "../common/components/Footer";
+
 
 const index = () => {
-  return <p className="text-3xl font-bold underline">Makesh Dev</p>;
+  return (
+    <>
+      <Header />
+      {/* Login */}
+      <div className="container mx-auto py-16 px-32">
+        <div className="grid grid-cols-2 gap-24">
+          <div>
+            <h3 className="text-2xl font-semibold mb-[30px]">Login</h3>
+            <div className="mb-[22px]">
+              <label className="mb-[20px] block font-medium">
+                Username Or Email Address
+              </label>
+              <input
+                type="email"
+                placeholder="Username/Email"
+                className="border border-1 border-oliveGreen-500 px-6 w-full h-[60px]"
+              />
+            </div>
+            <div className="mb-[22px]">
+              <label className="mb-[20px] block font-medium">Password</label>
+              <input
+                type="password"
+                placeholder="Password"
+                className="border border-1 border-oliveGreen-500 px-6 w-full h-[60px]"
+              />
+            </div>
+            <div className="flex justify-between items-center mb-[22px]">
+              <p>
+                <input type="checkbox" className="bg-primary mr-2" /> Remember
+                me.
+              </p>
+              <a href="">Forgot Password</a>
+            </div>
+            <div>
+              <button className="border-none bg-primary px-6 w-full h-[60px] text-white font-semibold">
+                Login
+              </button>
+            </div>
+          </div>
+
+          {/* Register */}
+          <div>
+            <h3 className="text-2xl font-semibold mb-[30px]">Register</h3>
+            <div className="mb-[22px]">
+              <label className="mb-[20px] block font-medium">Username</label>
+              <input
+                type="text"
+                placeholder="Username"
+                className="border border-1 border-oliveGreen-500 px-6 w-full h-[60px]"
+              />
+            </div>
+            <div className="mb-[22px]">
+              <label className="mb-[20px] block font-medium">
+                Email Address
+              </label>
+              <input
+                type="email"
+                placeholder="Password"
+                className="border border-1 border-oliveGreen-500 px-6 w-full h-[60px]"
+              />
+            </div>
+            <div className="mb-[22px]">
+              <label className="mb-[20px] block font-medium">Password</label>
+              <input
+                type="password"
+                placeholder="Password"
+                className="border border-1 border-oliveGreen-500 px-6 w-full h-[60px]"
+              />
+            </div>
+            <div className="mb-[22px]">
+              <p>
+                <input type="checkbox" className="bg-primary mr-2" /> Accept
+                terms & privacy policy
+              </p>
+            </div>
+            <div className="mb-[22px]">
+              <button className="border-none bg-primary px-6 w-full h-[60px] text-white font-semibold">
+                Register
+              </button>
+            </div>
+            <p className="text-center">
+              Already have an account?
+              <a href="" className="text-secondary pl-1">
+                Login
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <Footer/>
+    </>
+  );
 };
 
 export default index;
